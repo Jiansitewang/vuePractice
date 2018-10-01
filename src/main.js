59375b12e7b39cc697335fe7a976527efa7b35b4
Clay_wang <Clay_wang@vip.163.com>
2018-10-02 05:50:30 +0800

    回车增加todo、渲染todos进li、删除todo、已完成todo的样式绑定

--- src/main.js
+++ src/main.js
@@ -1,5 +1,3 @@
-// import Vue from 'vue'
-// import App from './App.vue'
 //
 // Vue.config.productionTip = false
 //
@@ -7,3 +5,32 @@
 //   render: h => h(App)
 // }).$mount('#app')
 import 'todomvc-app-css/index.css'
+import Vue from 'vue'
+
+new Vue({
+  el:'.todoapp',
+  data:{
+    msg:'',
+    todos:[{
+      content:'',
+      completed:false
+    }],
+  },
+  methods:{
+    addTodo(){
+      this.todos.push({
+        content:this.msg,
+        completed:false
+      })
+      this.msg = ''
+    },
+    removeTodo(index){
+      this.todos.splice(index,1)
+    }
+  }
+})
+
+// new Vue({
+//   el:".todoapp"
+//
+// })
