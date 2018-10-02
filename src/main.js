45e1bf538ba20e2da69f62cb1bf872acc562a8d5
Clay_wang <Clay_wang@vip.163.com>
2018-10-02 23:59:22 +0800

    computed属性

--- src/main.js
+++ src/main.js
@@ -7,14 +7,36 @@
 import 'todomvc-app-css/index.css'
 import Vue from 'vue'
 
+let filters = {
+  all(todos){
+    return todos
+  },
+  active(todos){
+    return todos.filter((todo)=>{
+      return !todo.completed
+    })
+  },
+  completed(todos){
+    return todos.filter((todo)=>{
+      return todo.completed
+    })
+  }
+}
+
 new Vue({
   el:'.todoapp',
   data:{
     msg:'',
-    todos:[{
-      content:'',
+    todos:[
+      {
+      content:null,
       completed:false
     }],
+  },
+  computed:{
+    remain(){
+      return filters.active(this.todos).length
+    }
   },
   methods:{
     addTodo(){
